dd69c73abc5a11bde05f4dcc30b52705e19dd119
xiaoyuhu13 <66574643+xiaoyuhu13@users.noreply.github.com>
2020-12-29 21:26:39 -0700

    Add files via upload
    finished popups, menu/hours & location popups still not working

---
+++ China-Gourmet-master/China-Gourmet-master/resources/scripts/script.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    dots: '#dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  })
+})
+
+
+/*idk about this part*/
+let soup = document.getElementById('soups');
+let glider = document.getElementsByClassName('glider');
+soup.onclick = function(){
+  glider.scrollTo(2);
+};
