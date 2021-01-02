f672e5c1ecd7e0289f57d83a8b36df6c36267362
ItsMe1324 <AndrewWu1324@gmail.com>
2021-01-02 16:31:45 -0700

    Idk how to fix this

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -12,3 +12,12 @@ window.addEventListener('load', function(){
 function myFunction() {
   document.getElementById("dots").classList.toggle("show");
 }
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("dots");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
