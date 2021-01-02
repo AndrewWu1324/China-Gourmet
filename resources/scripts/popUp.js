7dbd9f28a10630424d8d2056eef3db5488b4cb97
ItsMe1324 <AndrewWu1324@gmail.com>
2021-01-01 18:14:38 -0700

    Fixed it a bit

--- resources/scripts/popUp.js
+++ resources/scripts/popUp.js
@@ -1,6 +1,5 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-var modal2 = document.getElementById("myModal2");
 var modalC = document.getElementById("covidModal")
 
 // Get the button that opens the modal
@@ -10,17 +9,13 @@ var btn2 = document.getElementById("orderNowBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close2")[0];
-var spanC = document.getElementsByClassName("closeC")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 btn2.onclick = function() {
-  modal2.style.display = "block";
-}
-window.onload = function() {
-  modalC.style.display = "block"
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -28,8 +23,9 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 span2.onclick = function() {
-  modal2.style.display = "none";
-}
-spanC.onclick = function() {
   modalC.style.display = "none";
+}
+
+window.onload = function() {
+  modalC.style.display = "block"
 }
--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -9,29 +9,6 @@ window.addEventListener('load', function(){
   })
 })
 
-
-/*idk about this part*/
-soup = document.getElementById('soups');
-glider = document.getElementsByClassName('glider');
-soup.onclick = function(){
-  glider.scrollItem(2, true);
-};
-
-
 function myFunction() {
-  document.getElementById("glider-dots").classList.toggle("show");
+  document.getElementById("dots").classList.toggle("show");
 }
-
-/*
-window.onclick = function(event) {
-  if (!event.target.matches('.')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-} */
