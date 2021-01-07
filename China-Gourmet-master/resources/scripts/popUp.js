5d72ca160df906479e492b605b4c56d64897c4f7
ItsMe1324 <AndrewWu1324@gmail.com>
2021-01-06 23:40:57 -0700

    Update popUp.js

--- China-Gourmet-master/resources/scripts/popUp.js
+++ China-Gourmet-master/resources/scripts/popUp.js
@@ -3,12 +3,17 @@ var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("orderBtn");
+var btn2 = document.getElementById("orderBtnM");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modalM.style.display = "none";
   modal.style.display = "block";
 }
 
